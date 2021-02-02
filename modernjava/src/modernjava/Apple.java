f370ae8e88c7cf4268d76a866e2c8098e1c31594
akak4456 <akak4456@naver.com>
2021-02-02 19:13:40 +0900

    commit

--- modernjava/src/modernjava/Apple.java
+++ modernjava/src/modernjava/Apple.java
@@ -3,10 +3,12 @@ package modernjava;
 public class Apple {
 	private Integer weight = 0;
     private Color color;
+    private String country;
 
-    public Apple(int weight, Color color) {
+    public Apple(int weight, Color color,String country) {
       this.weight = weight;
       this.color = color;
+      this.country = country;
     }
 
     public Integer getWeight() {
@@ -25,9 +27,16 @@ public class Apple {
       this.color = color;
     }
 
+    public String getCountry() {
+    	return this.country;
+    }
+    
+    public void setCountry(String country) {
+    	this.country = country;
+    }
     @SuppressWarnings("boxing")
     @Override
     public String toString() {
-      return String.format("Apple{color='%s', weight=%d}", color, weight);
+      return String.format("Apple{color='%s', weight=%d, country='%s'}", color, weight,country);
     }
 }
--- modernjava/src/modernjava/Practice.java
+++ modernjava/src/modernjava/Practice.java
@@ -1,54 +1,58 @@
 package modernjava;
 
+import static java.util.Comparator.comparing;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import static java.util.Comparator.comparing;
+import java.util.function.Function;
 
 public class Practice {
 	public static void main(String[] args) {
-		List<Apple> inventory = Arrays.asList(
-		        new Apple(80, Color.GREEN),
-		        new Apple(155, Color.GREEN),
-		        new Apple(120, Color.RED)
-		    );
-		inventory.sort(comparing(Apple::getWeight));
+		List<Apple> inventory = Arrays.asList(new Apple(80, Color.GREEN, "KR"), new Apple(155, Color.GREEN, "KR"),
+				new Apple(155, Color.GREEN, "EN"), new Apple(120, Color.RED, "FR"));
+		inventory.sort(comparing(Apple::getWeight).reversed().thenComparing(Apple::getCountry));
 		System.out.println(inventory);
+		
+		Function<Integer,Integer> f = x -> x+1;
+		Function<Integer,Integer> g = x -> x*2;
+		Function<Integer,Integer> h = f.compose(g);
+		int result = h.apply(1);
+		System.out.println(result);
 	}
-	
-	public static List<Apple> filterApplesByColor(List<Apple> inventory,Color color){
+
+	public static List<Apple> filterApplesByColor(List<Apple> inventory, Color color) {
 		List<Apple> result = new ArrayList<>();
-		for(Apple apple:inventory) {
-			if(apple.getColor().equals(color)) {
+		for (Apple apple : inventory) {
+			if (apple.getColor().equals(color)) {
 				result.add(apple);
 			}
 		}
 		return result;
 	}
-	
-	public static List<Apple> filterApplesByWeight(List<Apple> inventory,int weight){
+
+	public static List<Apple> filterApplesByWeight(List<Apple> inventory, int weight) {
 		List<Apple> result = new ArrayList<>();
-		for(Apple apple:inventory) {
-			if(apple.getWeight() > weight) {
+		for (Apple apple : inventory) {
+			if (apple.getWeight() > weight) {
 				result.add(apple);
 			}
 		}
 		return result;
 	}
-	
-	public static List<Apple> filterApples(List<Apple> inventory, ApplePredicate p){
+
+	public static List<Apple> filterApples(List<Apple> inventory, ApplePredicate p) {
 		List<Apple> result = new ArrayList<>();
-		for(Apple apple:inventory) {
-			if(p.test(apple)) {
+		for (Apple apple : inventory) {
+			if (p.test(apple)) {
 				result.add(apple);
 			}
 		}
 		return result;
 	}
-	
+
 	public static void prettyPrintApple(List<Apple> inventory, AppleFormatter formatter) {
-		for(Apple apple:inventory) {
+		for (Apple apple : inventory) {
 			String output = formatter.accept(apple);
 			System.out.println(output);
 		}
