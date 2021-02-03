21622f7f194bd29b8bd3fd97050b21be910ba371
akak4456 <akak4456@naver.com>
2021-02-03 22:02:54 +0900

    final commit

--- modernjava/src/modernjava/Practice.java
+++ modernjava/src/modernjava/Practice.java
@@ -1,60 +1,20 @@
 package modernjava;
 
 import static java.util.Comparator.comparing;
-
+import static java.util.stream.Collectors.toList;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 public class Practice {
 	public static void main(String[] args) {
-		List<Apple> inventory = Arrays.asList(new Apple(80, Color.GREEN, "KR"), new Apple(155, Color.GREEN, "KR"),
-				new Apple(155, Color.GREEN, "EN"), new Apple(120, Color.RED, "FR"));
-		inventory.sort(comparing(Apple::getWeight).reversed().thenComparing(Apple::getCountry));
-		System.out.println(inventory);
-		
-		Function<Integer,Integer> f = x -> x+1;
-		Function<Integer,Integer> g = x -> x*2;
-		Function<Integer,Integer> h = f.compose(g);
-		int result = h.apply(1);
-		System.out.println(result);
+		List<Integer> numbers = List.of(1, 3, 7, 8, 10, 15, 4)
+		        .stream()
+		        .dropWhile(i -> i < 10)
+		        .collect(toList());
+		System.out.println(numbers);
 	}
 
-	public static List<Apple> filterApplesByColor(List<Apple> inventory, Color color) {
-		List<Apple> result = new ArrayList<>();
-		for (Apple apple : inventory) {
-			if (apple.getColor().equals(color)) {
-				result.add(apple);
-			}
-		}
-		return result;
-	}
-
-	public static List<Apple> filterApplesByWeight(List<Apple> inventory, int weight) {
-		List<Apple> result = new ArrayList<>();
-		for (Apple apple : inventory) {
-			if (apple.getWeight() > weight) {
-				result.add(apple);
-			}
-		}
-		return result;
-	}
-
-	public static List<Apple> filterApples(List<Apple> inventory, ApplePredicate p) {
-		List<Apple> result = new ArrayList<>();
-		for (Apple apple : inventory) {
-			if (p.test(apple)) {
-				result.add(apple);
-			}
-		}
-		return result;
-	}
-
-	public static void prettyPrintApple(List<Apple> inventory, AppleFormatter formatter) {
-		for (Apple apple : inventory) {
-			String output = formatter.accept(apple);
-			System.out.println(output);
-		}
-	}
 }
